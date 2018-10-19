a98e9cf4db2af8f6b9b8c93d37f796dec1d563a0
Michael Hayslip <Michael.Hayslip@phoenix.edu>
2018-10-19 16:13:43 -0700

    added findname

--- simple_api.py
+++ simple_api.py
@@ -1,19 +1,21 @@
 from flask import Flask, request, Blueprint
 from flask_restplus import Resource, Api, fields
 from flask_pymongo import PyMongo
+import uuid
 
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'flask-demo'
 app.config['MONGO_URI'] = 'mongodb://172.17.0.3:27017/flask-demo'
+
 blueprint = Blueprint('api', __name__, url_prefix='/api')
 api = Api(blueprint, doc='/documentation')
 app.register_blueprint(blueprint)
 
 mongo = PyMongo(app)
 
-names_model = api.model('NamesModel', {'name' : fields.String})
-
+names_model = api.model('NamesModel', {'name' : fields.String, 'name_id': fields.String})
+name_by_id_model = api.model('NameByIdModel', {'name_id' : fields.String})
 
 @api.route('/hello')
 class HelloWorld(Resource):
@@ -24,10 +26,10 @@ class HelloWorld(Resource):
 class SeedNames(Resource):
     def get(self):
         name = mongo.db.names
-        name.insert_one({"name" : "Michael"})
-        name.insert_one({"name" : "David"})
-        name.insert_one({"name" : "Joseph"})
-        name.insert_one({"name" : "Arnold"})
+        name.insert_one({"name" : "Michael", "name_id": str(uuid.uuid1())})
+        name.insert_one({"name" : "Sam", "name_id": str(uuid.uuid1())})
+        name.insert_one({"name" : "David", "name_id": str(uuid.uuid1())})
+
         return {"message": "Data successfully Stored"}, 201
 
 @api.route('/get_names')        
@@ -39,12 +41,22 @@ class GetNames(Resource):
 
         if names.estimated_document_count() != 0:
             for n in names.find({}):
-                print (n)
-                output.append({'name': n['name']})
+                print (n['name_id'], n['name'])
+                output.append({'name': n['name'], 'name_id' : n['name_id']})
             return output, 200
 
         return {'status': "No names available."}
+
+@api.route('/find_names/<name_id>')
+class FindNames(Resource):
+    @api.expect(name_by_id_model)
+    def get(self, name_id):
+        names = mongo.db.names
+        r = names.find_one({'name_id': name_id})
+        if r:
+            return {'name_id' : r['name_id'], 'name': r['name']} 
         
+        return { "dataset": [ {name_id : "Not found"}]}
 
 
 if __name__ == '__main__':
