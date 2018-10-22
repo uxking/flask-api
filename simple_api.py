a8bd442c24e2a05e5bedba4c36c12e3f02e77d2c
Michael Hayslip <Michael.Hayslip@phoenix.edu>
2018-10-22 16:36:22 -0700

    adding post

--- simple_api.py
+++ simple_api.py
@@ -15,14 +15,14 @@ app.register_blueprint(blueprint)
 mongo = PyMongo(app)
 
 names_model = api.model('NamesModel', {'name' : fields.String, 'name_id': fields.String})
-name_by_id_model = api.model('NameByIdModel', {'name_id' : fields.String})
+new_name_model = api.model('NewNameModel', {'name': fields.String})
 
 @api.route('/hello')
 class HelloWorld(Resource):
     def get(self):
         return {"message" : "Hello World"}
 
-@api.route('/seed_names')
+@api.route('/seed_data')
 class SeedNames(Resource):
     def get(self):
         name = mongo.db.names
@@ -31,6 +31,22 @@ class SeedNames(Resource):
         name.insert_one({"name" : "David", "name_id": str(uuid.uuid1())})
 
         return {"message": "Data successfully Stored"}, 201
+
+@api.route('/add_name')
+class AddName(Resource):
+    @api.expect(new_name_model, validate=True)
+    @api.marshal_with(names_model, envelope='dataset')
+    def post(self):
+        if request.json and 'name' in request.json:
+
+            new_name = {
+                'name' : request.json['name'], 
+                'name_id' : str(uuid.uuid1())
+            }
+            cursor = mongo.db.names
+            cursor.insert_one(new_name)
+            return new_name
+        return
 
 @api.route('/get_names')        
 class GetNames(Resource):
@@ -44,20 +60,17 @@ class GetNames(Resource):
                 print (n['name_id'], n['name'])
                 output.append({'name': n['name'], 'name_id' : n['name_id']})
             return output, 200
+        
 
-        return {'status': "No names available."}
-
-@api.route('/find_names/<name_id>')
+@api.route('/find_by_id/<name_id>')
 class FindNames(Resource):
-    @api.expect(name_by_id_model)
+    @api.marshal_with(names_model, envelope='dataset')
     def get(self, name_id):
         names = mongo.db.names
         r = names.find_one({'name_id': name_id})
         if r:
             return {'name_id' : r['name_id'], 'name': r['name']} 
         
-        return { "dataset": [ {name_id : "Not found"}]}
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
